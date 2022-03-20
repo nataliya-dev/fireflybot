63933bec99627663f1723254c7453b7cc7bceecb
Nataliya <23035717+nataliya-dev@users.noreply.github.com>
2022-03-20 13:18:22 -0600

    saving image from camera using opencv

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,19 +1,52 @@
-#include <librealsense2/rs.hpp>
 #include <iostream>
+#include <librealsense2/rs.hpp>
+#include <opencv2/opencv.hpp>
 
-int main(){
-	std::cout << "Hello World!" << std::endl;
-	rs2::pipeline p;
-	p.start();
+void initialize_config(rs2::config& config) {
+  config.disable_all_streams();  // save processing power
+  auto color_stream = RS2_STREAM_COLOR;
+  int width = 640;
+  int height = 480;
+  auto format = RS2_FORMAT_BGR8;
+  int framerate = 0;  // 0 = any
+  config.enable_stream(color_stream, width, height, format, framerate);
+}
 
-	rs2::frameset frames = p.wait_for_frames();
-	rs2::video_frame color = frames.get_color_frame();
+void print_frame_info(const rs2::video_frame& color) {
+  std::cout << "Received frame!" << std::endl;
+  int width = color.get_width();
+  int height = color.get_height();
 
-	int width = color.get_width();
-	int height = color.get_height();
+  std::cout << "width: " << width << std::endl;
+  std::cout << "height: " << height << std::endl;
+}
 
-	std::cout << "width: " << width << std::endl;
-	std::cout << "height: " << height << std::endl;
-	return 0;
+void save_image(const rs2::video_frame& color_frame) {
+  int width = color_frame.get_width();
+  int height = color_frame.get_height();
 
+  cv::Mat cv_img(cv::Size(width, height), CV_8UC3,
+                 (void*)color_frame.get_data(), cv::Mat::AUTO_STEP);
+  bool check = cv::imwrite("img.jpg", cv_img);
+
+  if (check == false) {
+    std::cout << "FAILED to save image" << std::endl;
+  }
+}
+
+int main() {
+  std::cout << "Starting fireflybot!" << std::endl;
+
+  rs2::config config;
+  initialize_config(config);
+
+  rs2::pipeline pipeline;
+  pipeline.start(config);
+
+  rs2::frameset frames = pipeline.wait_for_frames();
+  rs2::video_frame color_frame = frames.get_color_frame();
+  print_frame_info(color_frame);
+  save_image(color_frame);
+
+  return 0;
 }
